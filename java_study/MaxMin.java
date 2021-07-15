4abfa8c5adc7e6e95244cd2a803aa85902f23857
eilin32 <dkdlfosem22@naver.com>
2021-07-15 20:17:45 +0900

    최소최대(10818)

--- java_study/MaxMin.java
+++ java_study/MaxMin.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+/*
+최소, 최대
+input : long N (1<=N<=1000000) = 정수의 개수
+				long array num = 정수들
+output : 정수 중 최솟값, 최댓값 공백 구분 출력
+*/
 public class MaxMin {
 	public static void main(String[] args) {
-		
+		Scanner s = new Scanner(System.in);
+		long N = s.nextLong();
+		ArrayList<Long> num = new ArrayList<>();
+		for(int i =0; i<N; i++){
+			num.add(i,s.nextLong());
+		}
+		Collections.sort(num);
+		System.out.println(num.get(0)+" "+num.get(num.size()-1));
 	}
 }
