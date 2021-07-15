37fac95965ec131c28e3abf1df3d5450bdde0721
eilin32 <dkdlfosem22@naver.com>
2021-07-15 16:43:55 +0900

    약수구하기

---
+++ java_study/measure.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+/**
+ * measure
+ * 약수 구하기
+ * input : int N, int K
+ * output : N의 약수 중 K번째 숫자
+ */
+public class measure {
+
+	public static void main(String[] args) {
+		//input
+		Scanner s = new Scanner(System.in);
+		int N = s.nextInt();
+		int K = s.nextInt();
+		
+
+		//logic
+		ArrayList<Integer> m = new ArrayList<Integer>();
+		for(int i =1; i<N; i++){
+			if(N%i==0){
+				int j = N/i;
+				if(i<j){
+					m.add(i);
+					m.add(j);
+				}
+				else if(i==j){
+					m.add(i);
+				}
+				else{
+					break;
+				}
+			}
+		}
+		Collections.sort(m);
+		//System.out.println(m);
+		//output
+		if(m.size()>=K){
+			System.out.println(m.get(K-1));
+		}
+		else{
+			System.out.println(0);
+		}
+	}
+}
