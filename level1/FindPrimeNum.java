688af283a422470500b75c2ff4f3ca2c509ec8b4
eilin32 <dkdlfosem22@naver.com>
2021-07-16 20:02:24 +0900

    난이도별로 문서 정리

---
+++ level1/FindPrimeNum.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * 소수 찾기
+ * 
+ * input : int N int num(N가지 숫자)
+ * output : N개의 num 중 소수가 몇 개인지
+ */
+public class FindPrimeNum {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int N = s.nextInt();
+		int count =0;
+		for(int i=0;i<N;i++){
+			int num = s.nextInt();
+			//소수확인
+			if(FindPrime(num)){
+				count+=1;
+			}
+		}
+		System.out.println(count);
+	}
+	//num이 소수임을 판정하기 위해선 루트num 까지의 수 중 1을 제외하고 그 자연수의 약수가 있는지 확인하면 된다.
+	//즉, num보다 작은 숫자중 제곱 값이 num보다 작거나 같은 경우 나머지가 있는지 확인
+	public static Boolean FindPrime(int num){
+		if(num<2) return false;
+		for(int i=2;i*i<=num;i++){
+			if(num%i==0) return false;
+		}
+		return true;
+	}
+}
---
+++ level1/NthLargeNum.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * n번째 큰 수
+ * 
+ * input : int T(테스트 케이스) int n(공백으로 숫자 입력 10번)
+ * output : 크기가 10인 배열 숫자 중 3번째 큰 수
+ */
+public class NthLargeNum {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int T = s.nextInt();
+		while(T>0){
+			//테스트 케이스마다 새로 array 정의
+			int[] array=new int[10];
+			for(int i=0; i<10;i++){
+				array[i]=s.nextInt();
+			}
+			//내림차순 정렬
+			for(int i =0; i<10;i++){
+				for(int j =i+1; j<10;j++){
+					if(array[j]>array[i]){
+						int t=array[i];
+						array[i]=array[j];
+						array[j]=t;
+					}
+				}
+			}
+			//3번째 원소 출력
+			System.out.println(array[2]);
+			T-=1;
+		}
+		
+	}
+}
