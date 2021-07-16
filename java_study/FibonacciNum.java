be33bc62708e982216ee3f9b2551b2a126b08a7b
eilin32 <dkdlfosem22@naver.com>
2021-07-16 15:13:23 +0900

    피보나치수5(10870)

---
+++ java_study/FibonacciNum.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * 피보나치 수 5
+ * 
+ * input : int n (0<=n<=20)
+ * output : n번째 피보나치 수
+ */
+public class FibonacciNum {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int a = 0; // n-1
+		int b = 1; // n-2
+		int answer =0;
+		//3번째 숫자부터
+		if(n==0){
+			System.out.println(a);
+		}
+		else if(n==1){
+			System.out.println(b);
+		}
+		else{
+			for(int i =2; i<=n; i++){
+				answer = a+b; // n
+				a=b;
+				b=answer;
+			}
+			System.out.println(answer);
+		}
+	}
+}
