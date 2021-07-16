206de856e5437bbdf23cb69588fd61229c3337d3
eilin32 <dkdlfosem22@naver.com>
2021-07-16 18:16:13 +0900

    최대공약수와 최소공배수

---
+++ java_study/GcdLcm.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * 최대 공약수 최소 공배수
+ * input : int a int b
+ * output : gcd \n lcm
+ */
+public class GcdLcm {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		int gcd = GCD(a, b);
+		int lcm = (a*b)/gcd;
+		System.out.println(gcd);
+		System.out.println(lcm);
+	}
+	
+public static int GCD(int a, int b){
+	if(a%b == 0){
+		return b;
+	}
+	else return GCD(b, a%b);
+}
+}
