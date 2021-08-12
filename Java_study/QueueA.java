9657365985d68dfef19f75235f2a7e3b7fea37f0
eilin32 <dkdlfosem22@naver.com>
2021-08-12 22:30:31 +0900

    update files

--- Java_study/QueueA.java
+++ Java_study/QueueA.java
@@ -2,55 +2,64 @@ package Java_study;
 
 import java.util.NoSuchElementException;
 
-class Queue<T>{
-	class Node<T>{
+class Queue<T> {
+	class Node<T> {
 		private T data;
 		private Node<T> next;
 
-		public Node(T data){
+		public Node(T data) {
 			this.data = data;
 		}
 	}
 
+	// Queue 특성상 처음과 끝 노드를 설정
 	private Node<T> first;
 	private Node<T> last;
 
-	public void add(T item){
-		Node<T> t=new Node<T>(item);
-		if(last != null){
-			last.next = t;
+	// Queue 노드 추가
+	public void add(T item) {
+		Node<T> t = new Node<T>(item);
+		// last가 null이 아닐 때(저장된 데이터가 있을 경우)
+		if (last != null) {
+			last.next = t; // 마지막 노드 다음을 t노드로 설정
 		}
-		last = t;
-		if(first == null){
+		last = t; // 마지막 노드를 t로 설정
+
+		// 처음이 null일 때(앞 노드들이 다 빠져서 last가 first로 된 경우)
+		if (first == null) {
 			first = last;
 		}
 	}
 
-	public T remove(){
-		if (first == null){
+	// Queue 노드 삭제
+	public T remove() {
+		// 처음 노드가 없을 경우 예외 처리
+		if (first == null) {
 			throw new NoSuchElementException();
 		}
 
+		// first.data를 data(반환용)에 따로 저장한 다음 first를 first 다음 노드로 설정
 		T data = first.data;
 		first = first.next;
 
-		if(first == null){
+		if (first == null) {
 			last = null;
 		}
 		return data;
 	}
 
-	public T peek(){
-		if(first == null){
+	public T peek() {
+		if (first == null) {
 			throw new NoSuchElementException();
 		}
 		return first.data;
 	}
 
-	public boolean isEmpty(){
+	public boolean isEmpty() {
 		return first == null;
 	}
 }
+
 public class QueueA {
 	public static void main(String[] args) {
 		Queue<Integer> q = new Queue<Integer>();
--- Java_study/StackA.java
+++ Java_study/StackA.java
@@ -2,42 +2,47 @@ package Java_study;
 
 import java.util.EmptyStackException;
 
-class Stack<T>{
-	class Node<T>{
+class Stack<T> {
+	class Node<T> {
 		private T data;
 		private Node<T> next;
 
-		public Node(T data){
+		public Node(T data) {
 			this.data = data;
 		}
 	}
 
+	//맨 위에 있는 top 노드 설정
 	private Node<T> top;
 
-	public T pop(){
-		if(top==null){
+	//Stack top 노드 꺼내기 
+	public T pop() {
+		//스택에 아무것도 안 들어있을 경우 throw
+		if (top == null) {
 			throw new EmptyStackException();
 		}
 
-		T item = top.data;
-		top = top.next;
+		T item = top.data; //top 노드 데이터 불러오기
+		top = top.next; //top을 다음 노드로 설정하기
 		return item;
 	}
 
-	public void push(T item){
+	//Stack 노드 추가
+	public void push(T item) {
 		Node<T> t = new Node<T>(item);
-		t.next = top;
-		top = t;
+		t.next = top; // t의 다음 노드를 기존 top 노드로 설정
+		top = t; // top을 t 노드로 다시 설정
 	}
 
-	public T peek(){
-		if(top == null){
+	public T peek() {
+		if (top == null) {
 			throw new EmptyStackException();
 		}
 		return top.data;
 	}
 
-	public boolean isEmpty(){
+	//top이 null일 경우 true 반환
+	public boolean isEmpty() {
 		return top == null;
 	}
 }
